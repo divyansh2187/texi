d6d9c33bfafac0dcf632556ea3e08c3d647739bf
Divyanshu Sargara <divyanshbhati1227@gmail.com>
2026-02-19 17:35:23 +0530

    setup complete

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const app = require("./app");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`is working fine ${PORT}`);
+})
